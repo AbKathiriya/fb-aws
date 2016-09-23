07a5f5fa0b54fba1e4c6de4277ac0c50abf58b9e
abkathiriya <akashk@arista.com>
2016-09-23 16:09:09 +0530

    minor changes

--- myapp/views.py
+++ myapp/views.py
@@ -13,7 +13,7 @@ def index(request):
 # http://fbnotify-env.ap-south-1.elasticbeanstalk.com/fb/callback/
 @csrf_exempt
 def realtime_subscription_callback(request):
-    print (request)
+    print (request.content)
     if request.method == 'GET':
         if request.GET.get('hub.mode') == 'subscribe' and request.GET.get("hub.verify_token") == config.FACEBOOK_REALTIME_VERIFY_TOKEN:
             challenge = request.GET.get('hub.challenge')
