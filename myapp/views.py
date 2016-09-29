c5dd532dff544aacea43d54a81eb84e0ffd9aa4c
abkathiriya <akashk@arista.com>
2016-09-29 16:07:01 +0530

    minor changes

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,12 @@
-# import logging
+import logging
 import json
 from myapp import config
 from django.shortcuts import render
+from django.dispatch import Signal
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseForbidden
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 def index(request):
     return render(request, 'output.html', {'req' : request,'content':'This is the index method'})
@@ -16,30 +17,35 @@ def index(request):
 @csrf_exempt
 def realtime_subscription_callback(request):
     if request.method == 'GET':
-        return render(request, 'output.html', {'req' : request,'content':'This is callback function GET'})
+        #return render(request, 'output.html', {'req' : request,'content':'This is callback function GET'})
         if request.GET.get('hub.mode') == 'subscribe' and request.GET.get("hub.verify_token") == config.FACEBOOK_REALTIME_VERIFY_TOKEN:
             challenge = request.GET.get('hub.challenge')
             return HttpResponse(challenge, content_type='text/plain')
         else:
             return HttpResponse()
     elif request.method == 'POST':
-        return render(request, 'output.html', {'req' : request,'content':'This is callback function POST'})
-        # post_body = simplejson.loads(request.body)
-        # object_type = post_body.get('object')
-        # entries = post_body.get('entry', [])
-        # for entry in entries:
-        #     # trigger a new_facebook_change signal by each entry
-        #     try:
-        #         realtime_update.send(
-        #             sender=None,
-        #             object_type=object_type,
-        #             uid=entry['uid'],
-        #             changed_fields=entry['changed_fields'],
-        #             time=entry['time'],
-        #             request=request
-        #         )
-        #     except Exception:
-        #         logger.exception("happened an error handling the real-time update entry %s" % entry)
+        # return render(request, 'output.html', {'req' : request,'content':'This is callback function POST'})
+        post_body = json.loads(request.body)
+        object_type = post_body.get('object')
+        entries = post_body.get('entry', [])
+        for entry in entries:
+            # trigger a new_facebook_change signal by each entry
+            try:
+                realtime_update = Signal(providing_args=["object_type",
+                                         "uid",
+                                         "changed_fields",
+                                         "time",
+                                         "request"])
+                realtime_update.send(
+                    sender=None,
+                    object_type=object_type,
+                    uid=entry['uid'],
+                    changed_fields=entry['changed_fields'],
+                    time=entry['time'],
+                    request=request
+                )
+            except Exception:
+                logger.exception("happened an error handling the real-time update entry %s" % entry)
         return HttpResponse()
     else:
         return HttpResponseForbidden()
