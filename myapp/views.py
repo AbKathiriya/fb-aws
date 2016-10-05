283b1fd44148794bf5ed83f2e3636d252eab924f
abkathiriya <akashk@arista.com>
2016-10-05 16:37:33 +0530

    minor changes

--- myapp/views.py
+++ myapp/views.py
@@ -21,6 +21,7 @@ def realtime_subscription_callback(request):
     f = open('op.txt','w')
     if request.method == 'GET':
         f.write("callback->GET"+str(request.GET))
+        f.close()
         if request.GET.get('hub.mode') == 'subscribe' and request.GET.get("hub.verify_token") == config.FACEBOOK_REALTIME_VERIFY_TOKEN:
             challenge = request.GET.get('hub.challenge')
             return HttpResponse(challenge, content_type='text/plain')
@@ -29,26 +30,30 @@ def realtime_subscription_callback(request):
     elif request.method == 'POST':
         post_body = json.loads(request.POST)
         f.write("callback->POST"+str(post_body))
-        object_type = post_body.get('object')
-        entries = post_body.get('entry', [])
-        for entry in entries:
-            # trigger a new_facebook_change signal by each entry
-            try:
-                realtime_update = Signal(providing_args=["object_type",
-                                         "uid",
-                                         "changed_fields",
-                                         "time",
-                                         "request"])
-                realtime_update.send(
-                    sender=None,
-                    object_type=object_type,
-                    uid=entry['uid'],
-                    changed_fields=entry['changed_fields'],
-                    time=entry['time'],
-                    request=request
-                )
-            except Exception:
-                logger.exception("happened an error handling the real-time update entry %s" % entry)
+        f.close()
         return HttpResponse()
+        # object_type = post_body.get('object')
+        # entries = post_body.get('entry', [])
+        # for entry in entries:
+        #     # trigger a new_facebook_change signal by each entry
+        #     try:
+        #         realtime_update = Signal(providing_args=["object_type",
+        #                                  "uid",
+        #                                  "changed_fields",
+        #                                  "time",
+        #                                  "request"])
+        #         realtime_update.send(
+        #             sender=None,
+        #             object_type=object_type,
+        #             uid=entry['uid'],
+        #             changed_fields=entry['changed_fields'],
+        #             time=entry['time'],
+        #             request=request
+        #         )
+        #     except Exception:
+        #         logger.exception("happened an error handling the real-time update entry %s" % entry)
+        # return HttpResponse()
     else:
+        f.write("ERROR")
+        f.close()
         return HttpResponseForbidden()
